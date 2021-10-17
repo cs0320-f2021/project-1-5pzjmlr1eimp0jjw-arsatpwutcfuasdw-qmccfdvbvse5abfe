f5d555235b7689ad0aaf9b209048488c0ac65b62
michellel352 <77763074+michellel352@users.noreply.github.com>
2021-10-17 19:01:56 -0400

    KD tree bug fix

--- src/main/java/edu/brown/cs/student/main/kdtree/KDTreeBuilder.java
+++ src/main/java/edu/brown/cs/student/main/kdtree/KDTreeBuilder.java
@@ -88,7 +88,7 @@ public class KDTreeBuilder {
       parentNode.leftBranch = leftNode;
 
       if (rightNodes.isEmpty()) { rightNode = null; }
-      else { rightNode = this.buildTree(comparator, leftNodes, depth+1); }
+      else { rightNode = this.buildTree(comparator, rightNodes, depth+1); }
       parentNode.rightBranch = rightNode;
       return parentNode;
     }
